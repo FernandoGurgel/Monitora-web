196edee5e01784c4b06ecca44c829327a9ebef8c
Fernando gurgel <fag.gurgel@gmail.com>
2018-11-23 16:14:21 -0400

    serve

--- src/main/webapp/static/js/monitoria.js
+++ src/main/webapp/static/js/monitoria.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$.getJSON( "http://localhost:8080/api/aluno", function(data){
+	$.getJSON( "http://13.82.130.246:8080/api/aluno", function(data){
 		var nomes = []
 		for(x =0; x<data.length; x++){
 	    	nomes.push( data[x].nome);
